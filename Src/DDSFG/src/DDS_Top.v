dc11940f734377ebcf45b10fe21bef27bab3dea6
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-07-07 16:45:11 +0700

    add file

--- Src/DDSFG/src/DDS_Top.v
+++ Src/DDSFG/src/DDS_Top.v
@@ -2,10 +2,12 @@ module DDS_Top (
     input Ext_Clk_27
 );
 
-  PLL_top pll (
+  PLL_Top m_pll (
       .clkin(Ext_Clk_27),
-      //.reset (~PLL_Reset), 1'b0
-      //.clkout(PLL_Clock),
-      //.lock  (PLL_Lock)
+      .reset(1'b0)  //1'b0
+      //.clkout(),
+      //.lock(PLL_Lock)
   );
+
+  //Clk_div m_clk_div ();
 endmodule

5ead8a6c4ff3c4102481f219db60e58dac1f2c8d
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-07-07 22:56:50 +0700

    add SDPTemplete, code

---
+++ Src/DDSFG/src/Clk_div.v
@@ -0,0 +1,39 @@
+//File name : Clk_Div.v
+//Description : Clk_Div module for DDSFG.
+//Company : KMITL
+//Project : DDSFG
+//------------------- 
+//Version : 00.01              
+//Date :  06.07.2025         
+//Author : Kunanon Wanyen                
+//Remark : Creation File
+module Clk_Div (
+    input  wire Pll_Clk,
+    input  wire RESETn,
+    output wire Fg_Clk,
+    output wire Dac_Clk
+);
+
+  reg rFg_Clk;
+  reg rDac_Clk;
+
+  assign Fg_Clk   = rFg_Clk;
+  assign Dac_Clk = rDac_Clk;
+
+  always @(posedge Pll_Clk or negedge RESETn) begin : u_rFg_Clk
+    if (!RESETn) begin
+      rFg_Clk <= 1'd0;
+    end else begin
+      rFg_Clk <= ~rFg_Clk;
+    end
+  end
+
+  always @(negedge Pll_Clk or negedge RESETn) begin : u_rDac_Clk
+    if (!RESETn) begin
+      rDac_Clk <= 1'd0;
+    end else begin
+      rDac_Clk <= ~rDac_Clk;
+    end
+  end
+
+endmodule
--- Src/DDSFG/src/DDS_Top.v
+++ Src/DDSFG/src/DDS_Top.v
@@ -1,14 +1,37 @@
+//File name : DDS_Top.v
+//Description : Top module for DDSFG.
+//Company : KMITL
+//Project : DDSFG
+//------------------- 
+//Version : 00.01              
+//Date :  06.07.2025         
+//Author : Kunanon Wanyen                
+//Remark : Creation File
+//------------------- 
 module DDS_Top (
-    input  Ext_Clk_27,
-    output PLL_Clock,
-    output PLL_Lock
+    input wire Ext_Clk,
+    input wire Ext_RESETn,
+
+    //tmp
+    output wire wFg_Clk,
+    output wire wDac_Clk
 );
+  wire wPll_RESET;
+  wire wPll_clk;
+  wire wPll_Lock;
+  wire wFg_RESETn;
 
   PLL_Top m_pll (
-      .clkin (Ext_Clk_27),
-      .reset (1'b0),
-      .clkout(PLL_Clock),
-      .lock  (PLL_Lock)
+      .clkin (Ext_Clk),
+      .reset (1'b0), // Active High if use for sim 1'b0
+      .clkout(wPll_Clk),
+      .lock  (wPll_clk)
   );
 
+  Clk_Div m_clk_div (
+    .Pll_Clk(wPll_Clk),
+    .RESETn(wFg_RESETn),
+    .Fg_Clk(wFg_Clk),
+    .Dac_Clk(wDac_Clks)
+  );
 endmodule

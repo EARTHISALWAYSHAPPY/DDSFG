76812454b1e8bb9d9950b6f99f3f565304acec68
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-07-13 22:37:36 +0700

    add SDP&fix

--- Src/DDSFG/src/Osc_top.v
+++ Src/DDSFG/src/Osc_top.v
@@ -10,16 +10,18 @@
 // Remark       : Creation File
 //----------------------------------------//
 module Osc_Top (
-    input wire Fg_Clk,
-    input wire RESETn,
-    input wire Enable,
-    input wire Ready,
-    input wire [31:0] Init1,  //sin(b)
-    input wire [31:0] Init2,  //2cos(b)
+    input  wire        Fg_Clk,
+    input  wire        RESETn,
+    input  wire        Enable,
+    input  wire        Ready,
+    input  wire [31:0] Init1,   // sin(b)
+    input  wire [31:0] Init2,   // 2cos(b)
     output wire [31:0] Out1,
     output wire [31:0] Out2
 );
-
+  //----------------------------------------//
+  // Signal Declaration
+  //----------------------------------------//
   reg [31:0] a;
   reg [31:0] b;
   reg [63:0] c;
@@ -28,22 +30,31 @@ module Osc_Top (
   reg [31:0] rOut1;
   reg [31:0] rOut2;
 
+  //----------------------------------------//
+  // Output Declaration
+  //----------------------------------------//
+  assign Out1 = rOut1;
+  assign Out2 = rOut2;
 
-  assign Out1 = rOut1;  //seq
-  assign Out2 = rOut2;  //seq
+  //----------------------------------------//
+  // Process Declaration
+  //----------------------------------------//
+
+  // c = a * Out1
+  always @(*) begin : for_combination_from_Formula
+    c <= $signed(a) * $signed(Out1);
+  end
 
   always @(*) begin : u_Out1_a
     Out1_a <= c[60:29];
   end
 
+  // Out = Out1_a - Out2
   always @(*) begin : u_Out
     Out <= Out1_a - Out2;
   end
 
-  always @(*) begin : for_combination_from_Formula
-    c <= $signed(a) * $signed(Out1);
-  end
-
+  // for Out1
   always @(posedge Fg_Clk or negedge RESETn) begin : u_rOut1
     if (!RESETn) begin
       rOut1 <= 32'd0;
@@ -54,6 +65,7 @@ module Osc_Top (
     end
   end
 
+  // for Out2
   always @(posedge Fg_Clk or negedge RESETn) begin : u_rOut2
     if (!RESETn) begin
       rOut2 <= 32'd0;
@@ -64,7 +76,8 @@ module Osc_Top (
     end
   end
 
-  always @(posedge Fg_Clk or negedge RESETn) begin : u_a  // a = 2cos(B)
+  // a = 2cos(B)
+  always @(posedge Fg_Clk or negedge RESETn) begin : u_a
     if (!RESETn) begin
       a <= 31'd0;
     end else if (Ready) begin
@@ -72,4 +85,5 @@ module Osc_Top (
     end
   end
 
+  //----------------------------------------//
 endmodule
